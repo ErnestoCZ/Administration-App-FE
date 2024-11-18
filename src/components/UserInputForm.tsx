771e6da2454bc4ae153ad36443e97513c9024bd0
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-18 11:57:26 +0100

    updated onSubmitHandler to create new user via userAPI

--- src/components/UserInputForm.tsx
+++ src/components/UserInputForm.tsx
@@ -1,52 +1,74 @@
-import { Button, Flex, FormControl, FormLabel, Input, Select } from '@chakra-ui/react'
-import { DevTool } from '@hookform/devtools'
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from '@chakra-ui/react';
+import { DevTool } from '@hookform/devtools';
 import { FC } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { UserInputFormData } from '../models/types';
+import { addUser } from '../services/usersAPI';
+import { User } from '../models/types';
 
-enum Status{
-    active = 'active',
-    inactive = 'inactive',
+export const UserInputForm: FC = () => {
+  const { control, register, handleSubmit } = useForm<UserInputFormData>();
 
-}
-
-type UserInputFormData ={
-    firstName: string;
-    lastName: string;
-    status: Status;
-}
-
-export const UserInputForm : FC = () => {
-  const {control,register, handleSubmit} = useForm<UserInputFormData>();
-  
-  const onSubmitHandler: SubmitHandler<UserInputFormData> = (data: UserInputFormData) => {
-    console.log(data);
-  }
+  const onSubmitHandler: SubmitHandler<UserInputFormData> = async (
+    data: UserInputFormData,
+  ) => {
+    const createdUser: User = await addUser(data);
+    console.log(createdUser);
+  };
 
   return (
-<form onSubmit={handleSubmit(onSubmitHandler)}>
-
-  <FormControl background={'white'} p={2} flexDirection={'column'} onSubmit={handleSubmit(onSubmitHandler)}>
-    <Flex direction={'column'}>
-
-    <Flex direction={'column'}>
-        <FormLabel htmlFor='firstName'>First Name</FormLabel>
-        <Input type='text' id='firstName' {...register('firstName',{required:true})}/>
-        <FormLabel htmlFor='lastName'>Last Name</FormLabel>
-        <Input type='text' id='lastName' {...register("lastName", {required:true})}/>
-    </Flex>
-    <Flex direction={'row'} m={1}>
-    <Select {...register('status',{required:true})}>
-        <option value='active'>Active</option>
-        <option value='inactive'>Inactive</option>
-    </Select>
-    </Flex>
-      <Flex>
-        <Button type='reset' variant={'reset'} m={2} >Reset</Button>
-        <Button type='submit' variant={'submit'} m={2}>Add</Button>
-      </Flex>
-    </Flex>
-    <DevTool control={control}/>
-  </FormControl>
-</form>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
+      <FormControl
+        background={'white'}
+        p={2}
+        flexDirection={'column'}
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <Flex direction={'column'}>
+          <Flex direction={'column'}>
+            <FormLabel htmlFor="firstName">First Name</FormLabel>
+            <Input
+              type="text"
+              id="firstName"
+              {...register('firstName', { required: true })}
+            />
+            <FormLabel htmlFor="lastName">Last Name</FormLabel>
+            <Input
+              type="text"
+              id="lastName"
+              {...register('lastName', { required: true })}
+            />
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              type="email"
+              id="email"
+              {...register('email', { required: true })}
+            />
+          </Flex>
+          <Flex direction={'row'} m={1}>
+            <Select {...register('status', { required: true })}>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
+            </Select>
+          </Flex>
+          <Flex>
+            <Button type="reset" variant={'reset'} m={2}>
+              Reset
+            </Button>
+            <Button type="submit" variant={'submit'} m={2}>
+              Add
+            </Button>
+          </Flex>
+        </Flex>
+        <DevTool control={control} />
+      </FormControl>
+    </form>
+  );
+};
