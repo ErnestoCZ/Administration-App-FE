337c4cf9eb4262a68310b370e815390aaf5996c7
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-18 11:59:29 +0100

    loader is using the getUser API to fetch users from Backend

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -1,33 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { FC } from 'react'
-import { fakeUsers, User } from '../fakeData'
-import { UserInputForm } from '../components/UserInputForm'
-import { UserList } from '../components/UserList'
-import { UserListItem } from '../components/UserListItem'
-import {Box} from '@chakra-ui/react';
+import { createFileRoute } from '@tanstack/react-router';
+import { FC } from 'react';
+import { User } from '../models/types';
+import { UserInputForm } from '../components/UserInputForm';
+import { UserList } from '../components/UserList';
+import { UserListItem } from '../components/UserListItem';
+import { Box } from '@chakra-ui/react';
+import { getUsers } from '../services/usersAPI';
 
 export const Route = createFileRoute('/user')({
-  component: () => <UserPage/>,
-  loader: (): User[] => {
-    setTimeout(() => {(
-      console.log('fakeUsers:', fakeUsers)
-    )}, 1000)
-    return fakeUsers
+  component: () => <UserPage />,
+  loader: async (): Promise<User[]> => {
+    const fetchedUsers: User[] = await getUsers();
+    return fetchedUsers;
   },
-})
-
+});
 
 const UserPage: FC = () => {
   const users = Route.useLoaderData<User[]>();
   return (
     <>
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'flex-start'} flex={'1 1 auto'}  backgroundColor={'grey.100'}>
-        <UserInputForm/>
-          <UserList>
-            {users.map( (user) => (<UserListItem key={user.id} user={user}/>))}
-          </UserList>
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'flex-start'}
+        flex={'1 1 auto'}
+        backgroundColor={'grey.100'}
+      >
+        <UserInputForm />
+        <UserList>
+          {users.map((user) => (
+            <UserListItem key={user.id} user={user} />
+          ))}
+        </UserList>
       </Box>
-      
     </>
-  )
-}
+  );
+};
