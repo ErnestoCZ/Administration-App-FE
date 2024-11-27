bd903ada9e0bbe7e02897ea74e5b359eea66cda5
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-27 23:58:32 +0100

    new theme properties for Button styles

--- src/themes/StyledComponentsTheme.ts
+++ src/themes/StyledComponentsTheme.ts
@@ -1,10 +1,43 @@
-
 export const StyledComponentTheme = {
+  colors: {
+    primary: '#05d9f5',
+    secondary: '#05d485',
+    warning: '#f5d905',
+    danger: '#f50505',
 
-    colors: {
-        primary: '#05d9f5', 
-        secondary: '#05d485',
-
-    }
-    
+    Button: {
+      primary: {
+        background: '#05d9f5',
+        color: '#fff',
+        hover: {
+          background: '#05d485',
+          color: '#fff',
+        },
+      },
+      secondary: {
+        background: '#05d485',
+        color: '#fff',
+        hover: {
+          background: '#05d9f5',
+          color: '#fff',
+        },
+        edit: {
+          background: '#f5d905',
+          color: '#fff',
+          hover: {
+            background: '#f50505',
+            color: '#fff',
+          },
+        },
+        delete: {
+          background: '#f50505',
+          color: '#fff',
+          hover: {
+            background: '#f5d905',
+            color: '#fff',
+          },
+        },
+      },
+    },
+  },
 };
