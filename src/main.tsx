97675f6c7bca79f71124f7cca88c22d56ac4e5e0
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-27 12:13:09 +0100

    included ThemeProvider by styled components

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from 'styled-components';
+import { StyledComponentTheme } from './themes/StyledComponentsTheme.ts';
 scan({ enabled: true, log: true });
 
 const router = createRouter({ routeTree });
@@ -19,8 +21,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider theme={StyledComponentTheme}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
