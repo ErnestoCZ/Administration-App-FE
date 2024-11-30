266970187cf9853e1c0981c9b9e225cf88750222
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-30 20:40:45 +0100

    moved Styled to Header.tsx

--- src/components/styles/Header.styled.ts
+++ src/components/styles/Header.styled.ts
@@ -1,19 +1,5 @@
 import { styled, keyframes } from 'styled-components';
 
-export const StyledHeader = styled.div`
-  background: linear-gradient(to Bottom, #fff, #05d9f5);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  height: 10vh;
-  margin: 0px;
-  font-family: Parkisans;
-  font-size: 24px;
-  font-weight: 800;
-  color: grey;
-`;
-
 export const Logo = styled.img<{ $spinleft?: boolean }>`
   width: 50px;
   height: 50px;
