33de6e20e45a61c9f15e5499214451dcb8a51294
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-04 10:56:45 +0100

    added rendering of billings in list

--- src/routes/billings.tsx
+++ src/routes/billings.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { FC } from 'react';
 import { Billing, fakeBillings } from '../fakeData';
+import { Stack } from '@/components/Stack';
+import { styled } from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+import { useAllBillingsData } from '@/hooks/useAllBillingsData';
+import { Input } from '@/components/Input';
+import { Flex } from '@/components/Flex';
 export const Route = createFileRoute('/billings')({
   component: () => <BillingsPage />,
   loader: (): Billing[] => {
@@ -8,10 +15,31 @@ export const Route = createFileRoute('/billings')({
   },
 });
 
+const StyledContentBox = styled.div`
+  background: white;
+  border-radius: 5px;
+  width: 100%;
+  padding: 1rem;
+  opacity: 0.8;
+`;
+
 export const BillingsPage: FC = () => {
+  const { data, isLoading } = useAllBillingsData();
+
+  const { control } = useForm();
   return (
     <>
-      <div></div>
+      <Stack direction={'column'}>
+        <Input placeholder="Search for Billing" />
+        <StyledContentBox>
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            data?.map((billing) => <Flex>{billing.month}</Flex>)
+          )}
+        </StyledContentBox>
+      </Stack>
+      <DevTool control={control} />
     </>
   );
 };
