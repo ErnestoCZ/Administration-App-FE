4b80a3fe5f52d0c847eb807626acbe30c8c68710
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-06 00:07:42 +0100

    adjusted colors for theme

--- src/themes/StyledComponentsTheme.ts
+++ src/themes/StyledComponentsTheme.ts
@@ -1,13 +1,13 @@
 export const StyledComponentTheme = {
   colors: {
     primary: '#05d9f5',
-    secondary: '#05d485',
+    secondary: '#ffffff',
     warning: '#f5d905',
     danger: '#f50505',
 
     Button: {
       primary: {
-        background: '#05d9f5',
+        background: 'navy',
         color: '#fff',
         hover: {
           background: '#05d485',
@@ -21,22 +21,22 @@ export const StyledComponentTheme = {
           background: '#05d9f5',
           color: '#fff',
         },
-        edit: {
-          background: '#f5d905',
-          color: '#fff',
-          hover: {
-            background: '#f50505',
-            color: '#fff',
-          },
-        },
-        delete: {
+      },
+      edit: {
+        background: '#f5d905',
+        color: '#fff',
+        hover: {
           background: '#f50505',
           color: '#fff',
-          hover: {
-            background: '#f5d905',
-            color: '#fff',
-          },
         },
+      },
+    },
+    delete: {
+      background: '#f50505',
+      color: '#fff',
+      hover: {
+        background: '#f5d905',
+        color: '#fff',
       },
     },
   },
