36c814ee56f38c81b83b002a7a223503dc97d557
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-06 00:02:19 +0100

    added variations in style

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,21 +2,39 @@ import { FC, PropsWithChildren } from 'react';
 import { styled } from 'styled-components';
 
 type htmlFor = string;
+type colorScheme = 'primary' | 'secondary';
 
-export const StyledLabel = styled.label<{ $htmlFor: htmlFor }>`
+export const StyledLabel = styled.label<{
+  $htmlFor?: htmlFor;
+  $colorScheme?: colorScheme;
+}>`
   font-weight: bold;
   font-size: 1.2rem;
-  color: #000;
+  color: ${(props) => {
+    switch (props.$colorScheme) {
+      case 'primary':
+        return props.theme.colors.primary;
+      case 'secondary':
+        return props.theme.colors.secondary;
+    }
+    return props.theme.colors.primary;
+  }};
 `;
 
 interface LabelProps {
   children: React.ReactNode;
+  colorScheme?: colorScheme;
   htmlFor?: string;
 }
 
 export const Label: FC<PropsWithChildren<LabelProps>> = ({
   children,
   htmlFor,
+  colorScheme,
 }) => {
-  return <StyledLabel $htmlFor={`${htmlFor}`}>{children}</StyledLabel>;
+  return (
+    <StyledLabel $colorScheme={colorScheme} $htmlFor={htmlFor}>
+      {children}
+    </StyledLabel>
+  );
 };
