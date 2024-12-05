1de6aca202b2539252ee0ea9e768bff4c7ab51d2
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-06 00:00:14 +0100

    added Chakra Provider

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 import { StyledComponentTheme } from './themes/StyledComponentsTheme.ts';
+import { Provider } from './components/ui/provider.tsx';
 scan({ enabled: true, log: true });
 
 const router = createRouter({ routeTree });
@@ -21,10 +22,12 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={StyledComponentTheme}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
+      <Provider>
+        <ThemeProvider theme={StyledComponentTheme}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </Provider>
     </QueryClientProvider>
   </StrictMode>,
 );
