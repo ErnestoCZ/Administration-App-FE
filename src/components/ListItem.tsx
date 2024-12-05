7a7d1607f654bd334be92f5417f9af4128931ca9
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-06 00:02:56 +0100

    generic component renders with StyledListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,8 +1,20 @@
+import styled from 'styled-components';
+
+const StyledListItem = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  background: white;
+  border-radius: 5px;
+  width: 100%;
+  padding: 0.5rem;
+  opacity: 0.8;
+  margin-top: 1vh;
+`;
+
 interface ListItemProps<T> {
   item: T;
-  renderItem: (item: T) => JSX.Element;
+  renderItem: (item: T) => React.ReactNode;
 }
 
 export const ListItem = <T,>({ item, renderItem }: ListItemProps<T>) => {
-  return <div>{renderItem(item)}</div>;
+  return <StyledListItem>{renderItem(item)}</StyledListItem>;
 };
