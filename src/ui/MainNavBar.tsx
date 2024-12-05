9fceb93d1e0b2aeed2e1859821acb0384a85adbf
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-06 00:07:13 +0100

    removed boarder radius in MainNavBar

--- src/ui/MainNavBar.tsx
+++ src/ui/MainNavBar.tsx
@@ -6,7 +6,7 @@ export const StyledMainNavBar = styled.div`
   display: flex;
   flex-direction: row;
   background-color: #05d9f5;
-  border-radius: 3px;
+  border-radius: 0px;
   margin: 0px;
   padding: 10px;
   min-width: 100%;
