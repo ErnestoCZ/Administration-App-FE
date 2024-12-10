5138f73ec533820e3feab75247b8da5d910fec63
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-10 11:09:04 +0100

    added inital layout

--- src/routes/billing.$billingId.tsx
+++ src/routes/billing.$billingId.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Billing, fakeBillings } from '../fakeData';
-import { Stack } from '@/components/Stack';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
+import { Flex } from '@/components/Flex';
+import { Label } from '@/components/Label';
+import { Input } from '@/components/Input';
+import List from '@/components/List';
+import { Button } from '@/components/Button';
+import { UserSelect } from '@/components/UserSelect';
 export const Route = createFileRoute('/billing/$billingId')({
   loader: ({ params }): Billing | undefined => {
     const foundBilling = fakeBillings.find(
@@ -17,13 +22,37 @@ export const BillingByIdComponent = () => {
   const data = Route.useLoaderData();
 
   return (
-    <Stack direction={'column'}>
-      <Flex>
-        <div>{data?.month}</div>
-      </Flex>
-      <Flex>
-        <Box>{`From ${data?.dateFrom} to ${data?.dateTo}`}</Box>
-      </Flex>
-    </Stack>
+    <>
+      <Label>{data?.month}</Label>
+      <Stack direction={'row'}>
+        <Stack direction={'column'}>
+          <Label colorScheme="secondary">Details</Label>
+          <Flex direction="column">
+            <Box>{data?.month}</Box>
+            <Box display={'flex'} flexDirection={'column'}>
+              <Label colorScheme="secondary">From:</Label>
+              <Input type="text" value={`${data?.dateFrom}`} />
+            </Box>
+            <Box display={'flex'} flexDirection={'column'}>
+              <Label colorScheme="secondary">To:</Label>
+              <Input type="text" value={`${data?.dateTo}`} />
+            </Box>
+
+            <Box>
+              <Label colorScheme="secondary">Constants:</Label>
+              <p>Constant 1</p>
+              <p>Constant 2</p>
+              <p>Constant 3</p>
+            </Box>
+          </Flex>
+        </Stack>
+        <Stack direction={'column'}>
+          <Flex direction={'column'}>
+            <Button>Add User to Billing</Button>
+            <UserSelect />
+          </Flex>
+        </Stack>
+      </Stack>
+    </>
   );
 };
