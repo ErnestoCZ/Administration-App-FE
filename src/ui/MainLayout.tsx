cc6d149fe4931812fd09dad211cd659a057dc5b8
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-15 22:22:32 +0100

    updated mainlayout via managing styles in template directly

--- src/ui/MainLayout.tsx
+++ src/ui/MainLayout.tsx
@@ -2,29 +2,47 @@ import { FC } from 'react';
 import { Outlet } from '@tanstack/react-router';
 import { MainNavBar } from './MainNavBar.tsx';
 import reactLogo from '../assets/react.svg';
-import { Footer } from '@/components/Footer.tsx';
 import { Stack } from '@/components/Stack.tsx';
 import { Header } from '@/components/Header.tsx';
 import { Logo } from '@/components/Logo.tsx';
 import { HeaderTitle } from '@/components/HeaderTitle.tsx';
-import { OutletWrapper } from '@/components/OutletWrapper.tsx';
+import styled from 'styled-components';
+
+
+const StyledMainLayoutWrapper = styled.div`
+  background-color: orange;
+  height: 100%;
+`
+
+const StyledOutletWrapper = styled.div`
+  padding: 1vh;
+  width: 100%;
+  height: 70rem;
+`
 
 export const MainLayout: FC = () => {
   return (
     <>
-      <Stack direction={'column'}>
+    <StyledMainLayoutWrapper>
+
+      <Stack flexDirection='column'>
         <Header>
           <Logo src={reactLogo} alt={'logo'} spinleft={false} />
           <HeaderTitle title={'Administration App'} />
           <Logo src={reactLogo} alt={'logo'} spinleft={true} />
         </Header>
+          <MainNavBar />
 
-        <MainNavBar />
-        <OutletWrapper>
+        <StyledOutletWrapper>
           <Outlet />
-        </OutletWrapper>
-        <Footer></Footer>
+        </StyledOutletWrapper>
+        {/* <Footer>Footer</Footer> */}
       </Stack>
+
+
+
+
+    </StyledMainLayoutWrapper>
     </>
   );
 };
