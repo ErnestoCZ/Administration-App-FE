4ce44743bd1ab956e877d9372664b3389ea04647
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-15 22:27:11 +0100

    cleaned up theme

--- src/themes/StyledComponentsTheme.ts
+++ src/themes/StyledComponentsTheme.ts
@@ -1,43 +1,6 @@
 export const StyledComponentTheme = {
   colors: {
-    primary: '#05d9f5',
-    secondary: '#ffffff',
-    warning: '#f5d905',
-    danger: '#f50505',
-
-    Button: {
-      primary: {
-        background: 'navy',
-        color: '#fff',
-        hover: {
-          background: '#05d485',
-          color: '#fff',
-        },
-      },
-      secondary: {
-        background: '#05d485',
-        color: '#fff',
-        hover: {
-          background: '#05d9f5',
-          color: '#fff',
-        },
-      },
-      edit: {
-        background: '#f5d905',
-        color: '#fff',
-        hover: {
-          background: '#f50505',
-          color: '#fff',
-        },
-      },
-    },
-    delete: {
-      background: '#f50505',
-      color: '#fff',
-      hover: {
-        background: '#f5d905',
-        color: '#fff',
-      },
-    },
-  },
+    primary: "navy",
+    secondary: "none"
+  }
 };
