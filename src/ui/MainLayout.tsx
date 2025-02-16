664e578c2565944526308af4a8514a98b6aebc87
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-16 10:17:06 +0100

    adjusted styling

--- src/ui/MainLayout.tsx
+++ src/ui/MainLayout.tsx
@@ -11,13 +11,13 @@ import styled from 'styled-components';
 
 const StyledMainLayoutWrapper = styled.div`
   background-color: orange;
-  height: 100%;
+  height: 100rem;
 `
 
 const StyledOutletWrapper = styled.div`
   padding: 1vh;
   width: 100%;
-  height: 70rem;
+  height: 70%;
 `
 
 export const MainLayout: FC = () => {
