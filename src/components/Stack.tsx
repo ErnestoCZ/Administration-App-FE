bfdd0f2ec5ad2233060afac2c198896ce041d79d
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-16 10:25:14 +0100

    added new style properties to Stack

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -6,7 +6,13 @@ type DivProps = ComponentProps<'div'> & CSSProperties;
 
 const StyledStack = styled.div<DivProps>`
   display: flex;
-  flex-direction: ${(props) => props.flexDirection};
+  flex-direction: ${props => props.flexDirection ? props.flexDirection : 'row'};
+  justify-content: ${props => props.justifyContent};
+  justify-items: ${props => props.justifyItems};
+  justify-self: ${props => props.justifySelf};
+  align-content: ${props => props.alignContent};
+  align-items: ${props => props.alignItems};
+  align-self: ${props => props.alignSelf};
   width: 100%;
   height: 100%;
 `;
