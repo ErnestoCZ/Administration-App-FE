08efc87815c2742d1d1afb71c88520b0c213f181
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-16 10:15:51 +0100

    inital page structure including forms for control properties

--- src/routes/control.tsx
+++ src/routes/control.tsx
@@ -1,16 +1,25 @@
+import { Box } from '@/components/Box'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { Separator } from '@/components/Separator'
 import { Stack } from '@/components/Stack'
+import { InputController } from '@/Controller/InputController'
 import { createFileRoute } from '@tanstack/react-router'
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
 
 const StyledControlBox = styled.div`
   display: flex;
   flex-direction: row;
-  /* width: 100%;
-  height: 100%; */
-  background-color: aliceblue;
+  width: 100%;
+  height: 100%; 
+  background-color: white;
   justify-content: space-around;
+  position:relative;
 `
+
+
 
 export const Route = createFileRoute('/control')({
   component: () => <ControlPage/>,
@@ -18,12 +27,49 @@ export const Route = createFileRoute('/control')({
 
 
 export const ControlPage = () => {
+  const {control: userFormControl, handleSubmit : userHandleSubmit} = useForm();
+  const {control: constantsFormControl, handleSubmit: constantsHandleSubmit} = useForm();
+
   return (
     <StyledControlBox>
 
-     <Stack flexDirection='column'>
-      Forms
+     <Stack flexDirection='column' justifyContent='flex-start' alignItems='center'>
+
+      <Box>
+      <form>
+        <Stack flexDirection='column'>
+        <InputController control={userFormControl} name='firstName' type='text' defaulValue='' placeholder='firstName' ></InputController>
+        <InputController control={userFormControl} name='lastName' type='text' defaulValue='' placeholder='lastName' ></InputController>
+        <InputController control={userFormControl} name='email' type='email' defaulValue='' placeholder='email' ></InputController>
+        <Stack>
+          <Button type='reset'>Reset</Button>
+          <Button type='submit'>Submit</Button>
+        </Stack>
+
+        </Stack>
+
+      </form>
+      </Box>
+
+      <Box>
+      <form>
+        <Stack flexDirection='column'>
+          Form for Constants and other things corresponding to Billing
+        </Stack>
+      </form>
+
+      </Box>
       </Stack> 
+
+      <Stack flexDirection='column' justifyContent='flex-start' alignItems='center' >
+          <Box>
+          <Input type='text'/>
+
+          Render list of Users here!
+
+          </Box>
+
+      </Stack>
 
     </StyledControlBox>
   )
