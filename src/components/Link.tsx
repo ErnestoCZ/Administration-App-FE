2e0265202b24973ccf01e66ec3ea74a31b2d3cfb
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-19 18:02:50 +0100

    added tanstack link props

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,9 +1,8 @@
-import { Link as TanStackLink } from '@tanstack/react-router';
+import { Link as TanStackLink, LinkProps } from '@tanstack/react-router';
 import { styled } from 'styled-components';
 import { FC, PropsWithChildren } from 'react';
-import { LinkProps } from '@chakra-ui/react';
 
-export const StyledLink = styled(TanStackLink)`
+export const StyledLink = styled(TanStackLink)<LinkProps>`
   color: ${props => props.theme.colors.secondary};
   text-decoration: none;
   display: flex;
@@ -16,12 +15,7 @@ export const StyledLink = styled(TanStackLink)`
   }
 `;
 
-interface CustomLinkProps extends LinkProps {
-  to: string;
-  params?: Record<string, string>;
-}
-
-export const Link: FC<PropsWithChildren<CustomLinkProps>> = ({
+export const Link: FC<PropsWithChildren<LinkProps>> = ({
   children,
   ...rest
 }) => {
