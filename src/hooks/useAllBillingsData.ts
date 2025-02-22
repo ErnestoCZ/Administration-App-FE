39f5a1520f837676f1998026ba6dcee37686bbee
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-22 11:47:23 +0100

    deleted old api services writen by using fetch api

--- src/hooks/useAllBillingsData.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAllBillings } from '@/services/billingsAPI';
-
-export const useAllBillingsData = () => {
-  return useQuery({
-    queryKey: ['billings'],
-    queryFn: getAllBillings,
-  });
-};
