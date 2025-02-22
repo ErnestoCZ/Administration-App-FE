36b39624f4489c8a4057357a94e2cee37c3fc021
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-02-22 11:51:43 +0100

    removed some properties

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,19 +1,17 @@
-import { FC, PropsWithChildren, CSSProperties, ComponentProps } from "react"
-import styled from "styled-components"
+import { FC, PropsWithChildren, CSSProperties, ComponentProps } from 'react';
+import styled from 'styled-components';
 
 type BoxProps = ComponentProps<'div'> & CSSProperties;
 
 const StyledBox = styled.div<BoxProps>`
   width: 100%;
-  height: ${props => props.height || 'auto'};
-  background-color: ${props => props.backgroundColor ? props.backgroundColor : 'none'};
+  height: ${(props) => props.height || 'auto'};
+  background-color: white;
   padding: 1vh;
-  margin: ${props => props.margin};
-  border: 1px solid black
-`
+  margin: 0;
+  border: 1px solid black;
+`;
 
-export const Box : FC<PropsWithChildren<BoxProps>> = ({children, ...rest}) => {
-  return (
-    <StyledBox {...rest}>{children}</StyledBox>
-  )
-}
+export const Box: FC<PropsWithChildren<BoxProps>> = ({ children, ...rest }) => {
+  return <StyledBox {...rest}>{children}</StyledBox>;
+};
